1e7abf5019ec44347ff3a81f93c246dcd1e2064b
Jitesh <jiteshgopale26@gmail.com>
2025-09-04 18:16:43 +0530

    frontend project setup with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Sample from './components/Sample'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-red-300'>Hello, Multi Agent</h1>
+      <Sample/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div>
+      Demo Component
+    </div>
+  )
+}
+
+export default Sample
