103e1bfc24c0e22f50db5170781499d873698847
Jitesh <jiteshgopale26@gmail.com>
2025-09-09 12:54:50 +0530

    building otp based login ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import ChatPage from './components/ChatPage';
 import Login from './components/Login';
+import OTPLogin from './components/OTPLogin';
 import Register from './components/Register'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/chat" element={<ChatPage/>} />
+          <Route path="/otp-login" element={<OTPLogin/>} />
         </Routes>
       </Router>
     </>
---
+++ frontend/src/components/OTPLogin.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+const OTPLogin = () => {
+
+    const navigate = useNavigate();
+    const [phoneNo,setPhoneNo] = useState("");
+    const [otpSent,setOtpSent] = useState(false);
+    const [otp,setOtp] = useState("");
+    const [token, setToken]= useState("");
+    const [response,setResponse] = useState(null);
+
+    const handleSentOTP = async(e)=>{
+        e.preventDefault();
+
+        const res = await axios.post("http://localhost:5000/api/auth/otp-sent",{
+            phoneNo
+        },{
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+
+        const response = res.data;
+
+        if(response.message === "OTP Sent Sucessfully"){
+            setResponse(response.message)
+            console.log(response.message)
+            setToken(response.token)
+            setOtpSent(true)
+
+        }
+
+        
+    }
+    const handleVerifyOTP = async(e)=>{
+        e.preventDefault();
+
+        const res = await axios.post("http://localhost:5000/api/auth/otp-verify",{
+            otp
+        },{
+            headers:{
+                "Content-Type":"application/json",
+                "auth-token":token
+            }
+        })
+
+        const response = res.data;
+
+        if(response.message === "Login Successful"){
+            setResponse(response.message)
+            console.log(response.message)
+
+            navigate('/chat')
+        }
+
+        
+    }
+
+  return (
+    <>
+      <div className="otp">
+        <div className="flex flex-col justify-center items-center h-dvh">
+          <div className="border-2 p-5 rounded-2xl">
+            <form onSubmit={handleSentOTP}>
+              <div>
+                <h1 className="!text-4xl font-bold text-center mb-3">
+                  OTP Based LoginPage
+                </h1>
+              </div>
+
+              <div className="flex flex-col mb-2">
+                <label htmlFor="phoneNo">
+                  Phone No<span className="text-red-500">*</span>
+                </label>
+                <input
+                  value={phoneNo}
+                  type="text"
+                  id="phoneNo"
+                  name="phoneNo"
+                  placeholder="Enter your phoneNo"
+                  className="border border-gray-300 rounded-md"
+                  onChange={(e) => setPhoneNo(e.target.value)}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="border border-gray-700 w-full mt-2 rounded-md"
+              >
+                Submit
+              </button>
+              {otpSent && (
+                <>
+                  <div className="text-green-500 mt-2 text-center">
+                    <h1>OTP Sent Successfully</h1>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      className="w-full"
+                      placeholder="Enter OTP"
+                      value={otp}
+                      onChange={(e) => setOtp(e.target.value)}
+                    />
+                    <button className="border border-gray-700 w-full mt-2 rounded-md" onClick={handleVerifyOTP}>
+                      Verify OTP
+                    </button>
+                  </div>
+                </>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default OTPLogin
